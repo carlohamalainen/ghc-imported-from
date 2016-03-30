25e92e9cffb29a5ba2a4a1742ace62a5bdaab110
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-03-30 18:25:47 +0800

    Helper script.

---
+++ cp_to_dockers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cabal sdist
+cp dist/ghc-imported-from-*.tar.gz docker-testsuite/debian-cabal/
+cp dist/ghc-imported-from-*.tar.gz docker-testsuite/debian-stack/
+cp dist/ghc-imported-from-*.tar.gz docker-testsuite/fedora-cabal/
+cp dist/ghc-imported-from-*.tar.gz docker-testsuite/fedora-stack/
