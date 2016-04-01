60f3a4a7f5287c5fb8adb8139b99254a9fb68476
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-04-01 13:30:25 +0800

    Verbose cabal builds.

--- docker-testsuite/debian-cabal/build_and_test.sh
+++ docker-testsuite/debian-cabal/build_and_test.sh
@@ -5,7 +5,7 @@ set -x
 
 cabal update
 cabal sandbox init
-cabal install --enable-documentation --haddock-hyperlink-source --dependencies-only
-cabal install
-cabal configure --enable-tests && cabal build && cabal test
+cabal install --enable-documentation --haddock-hyperlink-source --dependencies-only --verbose
+cabal install --verbose
+cabal configure --enable-tests --verbose && cabal build --verbose && cabal test
 cat dist/test/ghc-imported-from-*-spec.log
--- docker-testsuite/ubuntu-cabal/build_and_test.sh
+++ docker-testsuite/ubuntu-cabal/build_and_test.sh
@@ -7,7 +7,7 @@ cabal update
 cabal install cabal-install
 export PATH=$HOME/.cabal/bin:$PATH
 cabal sandbox init
-cabal install --enable-documentation --haddock-hyperlink-source --dependencies-only
-cabal install
-cabal configure --enable-tests && cabal build && cabal test
+cabal install --enable-documentation --haddock-hyperlink-source --dependencies-only --verbose
+cabal install --verbose
+cabal configure --enable-tests --verbose && cabal build --verbose && cabal test
 cat dist/test/ghc-imported-from-*-spec.log
